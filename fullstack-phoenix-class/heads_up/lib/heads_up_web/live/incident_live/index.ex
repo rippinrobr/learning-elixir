bdee4a8d578c857d5065817b9ac5dd3ed4e8556a
Rob Rowe <rippinrobr@hotmail.com>
2025-02-16 07:06:23 -0500

    Finished 13. Listing Raffles section

---
+++ fullstack-phoenix-class/heads_up/lib/heads_up_web/live/incident_live/index.ex
@@ -0,0 +1,30 @@
+defmodule HeadsUpWeb.IncidentLive.Index do
+  use HeadsUpWeb, :live_view
+
+  alias HeadsUp.Incidents
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :incidents, Incidents.list_incidents())}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="incident-index">
+      <div class="incidents">
+        <div :for={incident <- @incidents} class="card">
+          <img src={incident.image_path} />
+          <h2>{incident.name}</h2>
+          <div class="details">
+            <div class="badge">
+              {incident.status}
+            </div>
+            <div class="priority">
+              {incident.priority}
+            </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    """
+  end
+end
